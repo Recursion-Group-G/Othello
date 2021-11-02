8acd4834fcddafea5eec1a1d073174adb860671f
Tucker <ohoka.taka@icloud.com>
2021-11-02 18:12:32 +0900

    PopUp.vueの更新

---
+++ src/components/PopUp.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-card>
+        <v-card-title>XX is Winner</v-card-title>
+
+        <v-card-actions>
+            <v-btn>Top</v-btn>
+            <v-btn>Retry?</v-btn>
+        </v-card-actions>
+    </v-card>
+
+</template>
+
+
+<script>
+
+</script>
+
+
+<sytle>
+</sytle>
