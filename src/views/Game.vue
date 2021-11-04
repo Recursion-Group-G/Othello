cd45456144965b74cfe0e769abcfdb7d94db5022
ta28ma2 <ta28ma2@gmail.com>
2021-11-04 21:55:33 +0900

    レスポンシブ対応完了

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,40 +1,37 @@
 <template>
     <v-app>
         <v-content class="v-content">    
-            <v-container>
+            <v-container class="d-flex justify-center text-center mt-5">
+            <!-- Players上部(スマホの時のみ表示) -->
+            <!-- Playerの配列は仮、プレイヤーの枚数"2"は後でプレイヤーの配列や点数の状態で書き換え -->
+                <h2 class="player-font" v-if="isXs">{{players[1]}}: 2</h2> 
             </v-container>
             <v-container class="board">
                 <v-row class="d-flex justify-center my-3">
                 <!-- Board -->
                     <div>
-                        <div
-                            v-for="i in board.size.row"
-                            :key="i"
-                            class="d-flex"
-                        >
-                            <div
-                                v-for="j in board.size.row"
-                                :key="j"
-                            >
-                                <div class="glass-square"></div>
+                        <div v-for="i in board.size.row" :key="i" class="d-flex">
+                            <div v-for="j in board.size.row" :key="j">
+                                <div class="board-square"></div>
                             </div>
                         </div>
                     </div>
-
                 </v-row>
             </v-container>
-            <!-- Players -->
+            <!-- Players下部 -->
             <v-container>
-                <v-row class="d-flex space-between text-center mb-5">
-                    <!-- v-forの"2"、Playerの名前、プレイヤーの枚数"2"は後でプレイヤーの配列や点数の状態で書き換え-->
-                    <v-col v-for="k in 2" 
-                        :key="k"
-                    >
-                    <h2 class="player-font">Player{{k}}: 2</h2> 
+                <v-row class="d-flex space-between text-center mb-5" v-if="!isXs">
+                    <!-- Playerの配列は仮、プレイヤーの枚数"2"は後で点数の状態で書き換え -->
+                    <v-col v-for="k in players" :key="k">
+                    <h2 class="player-font">{{k}}: 2</h2> 
+                    </v-col>
+                </v-row>
+                <v-row class="d-flex space-between text-center mb-5" v-else>
+                    <v-col>
+                    <h2 class="player-font">{{players[0]}}: 2</h2> 
                     </v-col>
                 </v-row>
             </v-container>
-            
         </v-content>
     </v-app>
 </template>
@@ -48,18 +45,24 @@ export default Vue.extend({
     },
     data: () => ({
         //後でconfig.tsに移行する
-        board: {
-            color:{
-                white: '#ffffff'
-            },
+        board:{
             size:{
                 row: 8,
-                col: 8
+                col: 8,
             }
         },
+        //仮のPlayer配列
+        players: ["Player1","Player2"],
     }),
 
+    computed: {
+        //スマホの画面判定
+        isXs(){
+            return this.$vuetify.breakpoint.name === 'xs';
+        },
+    }
 });
+
 </script>
 
 <style>
@@ -67,12 +70,10 @@ export default Vue.extend({
 @import url('https://fonts.googleapis.com/css2?family=Lato&display=swap');
 
 .v-content {
-    /*background-color: #ebebeb;*/
     background-color: #ffffff;
-  /*background-color: #00a86b;*/
 }
 
-.glass-square{
+.board-square{
     width: 90px;
     height: 90px;
     color: #ffffff;
@@ -81,7 +82,6 @@ export default Vue.extend({
     gap: 20px;
     border-radius: 0px;
     backdrop-filter: blur(9px);
-    /*background-color: rgba(255,255,255, 0.5);*/
     background-color: #09c15a;    
     box-shadow: rgba(0, 0, 0, 0.3) 2px 8px 8px;
     border: 6px rgba(255,255,255,0.4) solid;
@@ -89,31 +89,15 @@ export default Vue.extend({
     border-right: 6px rgba(40,40,40,0.35) solid;
 }
 
-.glass-square:hover{
+.board-square:hover{
     opacity: 0.2;
     background: #000000;
 }
-
-.neumorphism-square{
-    width: 100px;
-    height: 100px;
-    border-radius: 0px;
-/*
-    background: #09c15a;
-    box-shadow:  8px 8px 16px #4ea260,
-                -8px -8px 16px #88ffa6;
-*/
-    background: linear-gradient(145deg, #08ae51, #0acf60);
-    box-shadow:  8px 8px 16px #08ac50,
-                -8px -8px 16px #0ad664;
-}
-
 
 .player-font {
    font-family: "Lato";
    font-size: 40px;
 }
-
 
 @media screen and (max-width: 480px) {
     /* 480px以下に適用されるCSS（スマホ用） */
@@ -121,7 +105,7 @@ export default Vue.extend({
         width: 100%;
     }
 
-    .glass-square{
+    .board-square{
         width: 50px;
         height: 50px;
         color: #ffffff;
@@ -130,13 +114,11 @@ export default Vue.extend({
         gap: 20px;
         border-radius: 0px;
         backdrop-filter: blur(9px);
-        /*background-color: rgba(255,255,255, 0.5);*/
         background-color: #09c15a;    
         box-shadow: rgba(0, 0, 0, 0.3) 2px 8px 8px;
         border: 3.36px rgba(255,255,255,0.4) solid;
         border-bottom: 3.36px rgba(40,40,40,0.35) solid;
         border-right: 3.36px rgba(40,40,40,0.35) solid;
     }
-
 }
 </style>
