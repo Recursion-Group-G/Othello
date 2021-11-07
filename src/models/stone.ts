5cdaa1e574b4a9feab2387956b747307e5147e91
Tucker <ohoka.taka@icloud.com>
2021-11-07 16:22:20 +0900

    stone.tsの状態とメソッドの列挙

---
+++ src/models/stone.ts
@@ -0,0 +1,10 @@
+Stone: {
+    color: string
+    
+    color: number //null==石をおけない、1==白、2==黒、0==石なし(置ける)
+    //colorはどっち？
+    changeStoneNull() //nullにセットする
+    changeStoneZero() //0にセットする
+    changeStoneWhite() //1にセットする
+    changeStoneBlack() //2にセットする
+}
