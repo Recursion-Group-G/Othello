ac67fdac77a232dbd2770561c75f46f021963aa6
Leandro <leandro83g@icloud.com>
2021-11-08 22:56:13 +0900

    feat: Stone モデル作成

--- src/models/stone.ts
+++ src/models/stone.ts
@@ -1,10 +1,8 @@
-Stone: {
+class Stone{
     color: string
-    
-    color: number //null==石をおけない、1==白、2==黒、0==石なし(置ける)
-    //colorはどっち？
-    changeStoneNull() //nullにセットする
-    changeStoneZero() //0にセットする
-    changeStoneWhite() //1にセットする
-    changeStoneBlack() //2にセットする
+    constructor(color: string) {
+        this.color = color;
+    }
 }
+
+export default Stone;
