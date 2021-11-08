dfe4f6dc2b139ea71ff34fd0a88e816baf636455
kuro <toshras22@gmail.com>
2021-11-08 18:07:33 +0900

    Playermodel作成

---
+++ src/models/player.ts
@@ -0,0 +1,13 @@
+class Player {
+    name: string
+    score: number
+    color: string 
+    isCpu: boolean
+
+    constructor(name: string, score: number, color: string, isCpu: boolean) {
+        this.name = name;
+        this.score = score;
+        this.color = color;
+        this.isCpu = isCpu;
+    }
+}
