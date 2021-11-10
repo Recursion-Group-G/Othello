dac0c13114e647da81f5180bd45ffe58e9498e05
kuro <toshras22@gmail.com>
2021-11-10 22:36:10 +0900

    feat: board.tsを作成

---
+++ src/models/board.ts
@@ -0,0 +1,18 @@
+import Square from './square';
+interface Size {
+    x: number
+    y: number
+}
+
+class Board {
+    size: Size
+    squares: Square[][]
+
+    constructor(size: Size, squares: Square[][]) {
+        this.size = size;
+        this.squares = squares;
+    }
+}
+
+export default Board;
+
