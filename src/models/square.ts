521f71eb3a6cd7325b2748f793c959abcb333dbe
kuro <toshras22@gmail.com>
2021-11-10 22:45:34 +0900

    feat: squareに8方向のプロパティを追加

--- src/models/square.ts
+++ src/models/square.ts
@@ -10,10 +10,30 @@ class Square {
     stone: Stone | null;
     isEmpty: boolean;
 
+    top: Square | null 
+    right: Square | null 
+    bottom: Square | null 
+    left: Square | null
+    
+    topRight: Square | null 
+    topLeft: Square | null 
+    bottomRight: Square | null 
+    bottomLeft: Square | null 
+
+
     constructor(point: Point) {
         this.point = point;
         this.stone = null;
         this.isEmpty = false;
+
+        this.top = null;
+        this.right = null;
+        this.bottom = null;
+        this.left = null;
+        this.topRight = null; 
+        this.topLeft = null;
+        this.bottomRight = null;
+        this.bottomLeft = null;
     }
 }
 
