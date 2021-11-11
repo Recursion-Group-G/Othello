c61ed317b7b1ed37faeaaeaf4e5e07b626b7515d
Tucker <ohoka.taka@icloud.com>
2021-11-12 00:08:41 +0900

    PopUpをrouter.tsから削除

--- src/router.ts
+++ src/router.ts
@@ -3,16 +3,14 @@ import VueRouter from 'vue-router';
 import Game from './views/Game.vue';
 import Login from './views/Login.vue';
 import Top from './views/Top.vue';
-import PopUp from '.views/PopUp.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
     { path: '/', component: Top },
     { path: '/login', component: Login },
-    { path: '/game', component: Game },
-    { path: '/popup', component: PopUp}
-];
+    { path: '/game', component: Game }
+}
 
 const router = new VueRouter({
     mode: 'history',
