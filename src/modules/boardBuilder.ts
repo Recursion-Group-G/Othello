d96ecbdd3ead1be37a164dee7557ef34af73b959
Tucker <ohoka.taka@icloud.com>
2021-11-13 19:16:23 +0900

    boardBuilder.tsの変更

--- src/modules/boardBuilder.ts
+++ src/modules/boardBuilder.ts
@@ -1,19 +1,26 @@
+import Square from "../models/square";
+import Board from "../models/board";
+interface Size {
+    x: number;
+    y: number;
+}
+
 class BoardBuilder{
-    size;//ボードのサイズ、幅と高さ、x,y　自分用メモ
-    squares;
+    size: Size//ボードのサイズ、幅と高さ、x,y　自分用メモ
+    squares: Square[][];
 
     constructor(){
         this.reset();
     }
 
-    setSize(size){
+    setSize(size: Size){
         this.size = size;
         return this;
     }
 
     setSquares(){
         //linkを作成中
-        let squares = [];
+        let squares : Square[][] = [];
 
 
 
