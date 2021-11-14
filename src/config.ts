9e1359b81913acbc485da6f59fa45ae57f050a54
kuro <toshras22@gmail.com>
2021-11-14 21:39:02 +0900

    feat:interfaces/config.tsの作成

--- src/config.ts
+++ src/config.ts
@@ -1,39 +1,4 @@
-interface Config {
-    top: {
-        modes: { [key: string]: string }[];
-    };
-    stone: {
-        color: {
-            [key: string]: {
-                code: string;
-                id: number;
-            };
-        };
-    };
-    board: {
-        size: {
-            [key: string]: number;
-        };
-    };
-    square: {
-        size: {
-            [key: string]: number;
-        };
-    };
-    player: {
-        number: {
-            [key: string]: number;
-        };
-        initialScore: number;
-        playerIndex: number;
-        cpuIndex: number;
-    };
-    table: {
-        phase: {
-            [key: string]: number;
-        };
-    };
-}
+import Config from "./interfaces/config";
 
 const config: Config = {
     top: {
