34be20b6b7ea423e096650cf3a17046f7fabdb05
Tucker <ohoka.taka@icloud.com>
2021-11-14 22:59:55 +0900

    publicをメソッドに追加

--- src/modules/boardBuilder.ts
+++ src/modules/boardBuilder.ts
@@ -14,12 +14,12 @@ class BoardBuilder {
         this.squares = [];
     }
 
-    setSize(size: Size): BoardBuilder {
+    public setSize(size: Size): BoardBuilder {
         this.size = size;
         return this;
     }
 
-    setSquares(): BoardBuilder {
+    public setSquares(): BoardBuilder {
         let squares : Square[][] = [];
         for(let y : number = 0; y < this.size.y; y++){
             let squareX : Square[] = [];
@@ -50,11 +50,11 @@ class BoardBuilder {
         return this;
     }
 
-    build(): Board {
+    public build(): Board {
         return new Board(this.size, this.squares);
     }
 
-    reset(): void {
+    public reset(): void {
         this.size = {x: 0, y: 0};
         this.squares = [];
     }
