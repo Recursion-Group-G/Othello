744f967398cb28594d8a7250f99d26268b7e5981
Tucker <ohoka.taka@icloud.com>
2021-11-15 15:35:25 +0900

    レビューの修正

--- src/modules/boardBuilder.ts
+++ src/modules/boardBuilder.ts
@@ -9,7 +9,7 @@ class BoardBuilder {
     private size: Size;
     private squares: Square[][];
 
-    constructor() {
+    public constructor() {
         this.size = {x: 0, y: 0};
         this.squares = [];
     }
@@ -25,7 +25,7 @@ class BoardBuilder {
     }
 
     public createSquares(): BoardBuilder {
-        /**　squares[y][x]
+        /**  squares[y][x] -> (x,y)
          *   (0,0)
          *   *____________
          *   |__|__|__|__|
@@ -54,27 +54,33 @@ class BoardBuilder {
         for(let y : number = 0; y < this.size.y; y++){
             for(let x : number = 0; x < this.size.x; x++){
                 let curr : Square = this.squares[y][x];
+
+                //currから見た方向
+                const top : number= y - 1;
+                const right : number = x + 1;
+                const left : number = x - 1;
+                const bottom : number = y + 1;
                 
-                const canInsertTop = y > 0;
-                const canInsertRight = x < this.size.x - 1;
-                const canInsertButtom = y < this.size.y - 1;
-                const canInsertLeft = x > 0;
+                const canInsertTop : boolean = y > 0;
+                const canInsertRight : boolean = x < this.size.x - 1;
+                const canInsertButtom : boolean = y < this.size.y - 1;
+                const canInsertLeft : boolean = x > 0;
                 //縦横4方向の連結
-                if(canInsertTop) curr.top = this.squares[y - 1][x];
-                if(canInsertRight) curr.right = this.squares[y][x + 1];
-                if(canInsertButtom) curr.bottom = this.squares[y + 1][x];
-                if(canInsertLeft) curr.left = this.squares[y][x - 1];
+                if(canInsertTop) curr.top = this.squares[top][x];
+                if(canInsertRight) curr.right = this.squares[y][right];
+                if(canInsertButtom) curr.bottom = this.squares[bottom][x];
+                if(canInsertLeft) curr.left = this.squares[y][left];
 
 
-                const canInsertTopRight = x < this.size.x - 1 && y > 0;
-                const canInsertTopLeft = x > 0 && y > 0;
-                const canInsertBottomRight = x < this.size.x - 1 && y < this.size.y - 1;
-                const canInsertBottomLeft = x > 0 && y < this.size.y - 1;
+                const canInsertTopRight : boolean = x < this.size.x - 1 && y > 0;
+                const canInsertTopLeft : boolean = x > 0 && y > 0;
+                const canInsertBottomRight : boolean = x < this.size.x - 1 && y < this.size.y - 1;
+                const canInsertBottomLeft : boolean = x > 0 && y < this.size.y - 1;
                 //斜め4方向の連結
-                if(canInsertTopRight) curr.topRight = this.squares[y - 1][x + 1];
-                if(canInsertTopLeft) curr.topLeft = this.squares[y - 1][x - 1];
-                if(canInsertBottomRight) curr.bottomRight = this.squares[y + 1][x + 1];
-                if(canInsertBottomLeft) curr.bottomLeft = this.squares[y + 1][x - 1];
+                if(canInsertTopRight) curr.topRight = this.squares[top][right];
+                if(canInsertTopLeft) curr.topLeft = this.squares[top][left];
+                if(canInsertBottomRight) curr.bottomRight = this.squares[bottom][right];
+                if(canInsertBottomLeft) curr.bottomLeft = this.squares[bottom][left];
             }
         }
         return this;
