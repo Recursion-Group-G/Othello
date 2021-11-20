292fe08264fba06feeeae5c26ac28b94957c7e36
ta28ma2 <ta28ma2@gmail.com>
2021-11-20 20:05:02 +0900

    run lint

--- src/modules/enclosureController.ts
+++ src/modules/enclosureController.ts
@@ -5,7 +5,7 @@ import Enclosure from '@/models/enclosure';
 class EnclosureController {
     private head: Enclosure | null;
     private hashmap: { [key: string]: Enclosure | null };
-    
+
     public constructor() {
         this.head = null;
         this.hashmap = {};
@@ -41,13 +41,13 @@ class EnclosureController {
         this.updateFromSquare(topLeftSquare);
         this.updateFromSquare(topRightSquare);
         this.updateFromSquare(bottomLeftSquare);
-        this.updateFromSquare(bottomRightSquare)
+        this.updateFromSquare(bottomRightSquare);
         return this;
     }
 
     public addEnclosure(square: Square): void {
         const enclosure: Enclosure = new Enclosure(square);
-        if(this.head === null) this.head = enclosure;
+        if (this.head === null) this.head = enclosure;
         this.head.prev = enclosure;
         enclosure.next = this.head;
         this.head = enclosure;
@@ -56,11 +56,16 @@ class EnclosureController {
     }
 
     public removeEnclosure(square: Square): void {
-        if(this.head === null) return;
-        if(square === this.head.data) this.popFrontEnclosure();
+        if (this.head === null) return;
+        if (square === this.head.data) this.popFrontEnclosure();
         const enclosure: Enclosure | null = this.hashmap[square.id];
         //nullの表示エラー回避
-        if(enclosure === null || enclosure.prev === null || enclosure.next === null) return;
+        if (
+            enclosure === null ||
+            enclosure.prev === null ||
+            enclosure.next === null
+        )
+            return;
         enclosure.prev.next = enclosure.next;
         enclosure.next.prev = enclosure.prev;
         //hashmapから削除
@@ -69,44 +74,44 @@ class EnclosureController {
         //this.hashmap[square.id] = null;
     }
 
-    private popFrontEnclosure(): void{
+    private popFrontEnclosure(): void {
         this.head = this.head != null ? this.head.next : null; //nullの表示エラー回避
-        if(this.head != null) this.head.prev = null; //nullの表示エラー回避
+        if (this.head != null) this.head.prev = null; //nullの表示エラー回避
     }
 
     public getEnclosure(square: Square): Enclosure | null {
         return this.hashmap[square.id];
     }
 
-    public updateFromSquare(square: Square): void{
+    public updateFromSquare(square: Square): void {
         const isPresentInHashmap = (square: Square) => {
             return this.hashmap[square.id] != undefined;
-        }
+        };
         const isStoneEmpty = (square: Square) => {
             return square.isEmpty;
-        }
+        };
         const updateSquare = (square: Square) => {
             //hashmapにEnclosureがなくSquareに石がない場合
-            if(!isPresentInHashmap(square) && isStoneEmpty(square)) {
+            if (!isPresentInHashmap(square) && isStoneEmpty(square)) {
                 this.addEnclosure(square);
             }
             //hashmapにEnclosureがあってSquareに石がある場合
-            else if(isPresentInHashmap(square) && !isStoneEmpty(square)) {
+            else if (isPresentInHashmap(square) && !isStoneEmpty(square)) {
                 this.removeEnclosure(square);
             }
-        }
+        };
         updateSquare(square);
         //8方向にアップデート
         //nullの表示エラー回避
-        if(square.top != null) updateSquare(square.top); 
-        if(square.left != null) updateSquare(square.left)
-        if(square.right != null) updateSquare(square.right)
-        if(square.bottom != null) updateSquare(square.bottom)
+        if (square.top != null) updateSquare(square.top);
+        if (square.left != null) updateSquare(square.left);
+        if (square.right != null) updateSquare(square.right);
+        if (square.bottom != null) updateSquare(square.bottom);
 
-        if(square.topLeft != null) updateSquare(square.topLeft);
-        if(square.topRight != null) updateSquare(square.topRight);
-        if(square.bottomLeft != null) updateSquare(square.bottomLeft);
-        if(square.bottomRight != null) updateSquare(square.bottomRight);
+        if (square.topLeft != null) updateSquare(square.topLeft);
+        if (square.topRight != null) updateSquare(square.topRight);
+        if (square.bottomLeft != null) updateSquare(square.bottomLeft);
+        if (square.bottomRight != null) updateSquare(square.bottomRight);
     }
 }
 
