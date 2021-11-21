5cbab3222f91d31e64ed803c8f8f7436cb2f4a2e
Tucker <ohoka.taka@icloud.com>
2021-11-21 14:38:05 +0900

    boardBuilder.tsの修正

--- src/modules/boardBuilder.ts
+++ src/modules/boardBuilder.ts
@@ -1,14 +1,17 @@
 import Square from '../models/square';
 import Board from '../models/board';
 import Size from '../interfaces/size';
+import EnclosureController from '/enclosureController';
 
 class BoardBuilder {
     private size: Size;
     private squares: Square[][];
+    private enclosureController: EnclosureController;
 
     public constructor() {
         this.size = { x: 0, y: 0 };
         this.squares = [];
+        this.enclosureController = null;
     }
 
     public setSize(size: Size): BoardBuilder {
@@ -18,6 +21,11 @@ class BoardBuilder {
 
     public setSquares(squares: Square[][]): BoardBuilder {
         this.squares = squares;
+        return this;
+    }
+
+    public setEnclosureController(enclosureController: EnclosureController): BoardBuilder{
+        this.enclosureController = enclosureController;
         return this;
     }
 
@@ -90,12 +98,13 @@ class BoardBuilder {
     }
 
     public build(): Board {
-        return new Board(this.size, this.squares);
+        return new Board(this.size, this.squares, this.enclosureController);
     }
 
     public reset(): BoardBuilder {
         this.size = { x: 0, y: 0 };
         this.squares = [];
+        this.enclosureController = null;
         return this;
     }
 }
