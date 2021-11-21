e0b5cba59f6f1e0a233921dc6b5d332cae0a0b73
Tucker <ohoka.taka@icloud.com>
2021-11-21 14:24:49 +0900

    board.tsにenclosureControllerを追加

--- src/models/board.ts
+++ src/models/board.ts
@@ -1,13 +1,16 @@
 import Square from './square';
 import Size from '../interfaces/size';
+import EnclosureController from './src/modules/enclosureController';
 
 class Board {
     size: Size;
     squares: Square[][];
+    enclosureController: EnclosureController;
 
-    constructor(size: Size, squares: Square[][]) {
+    constructor(size: Size, squares: Square[][], enclosureController: EnclosureController) {
         this.size = size;
         this.squares = squares;
+        this.enclosureController = enclosureController;
     }
 }
 
