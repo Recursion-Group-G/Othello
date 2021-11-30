f9104dd48bfa47d73e2bd402a55d4503126cd0b4
Tucker <ohoka.taka@icloud.com>
2021-11-30 23:10:43 +0900

    不要なファイルを削除

--- src/models/stone.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var Stone = /** @class */ (function () {
-    function Stone(color) {
-        this.color = color;
-    }
-    return Stone;
-}());
-exports["default"] = Stone;
